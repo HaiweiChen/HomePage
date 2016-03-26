3e4ab7f54a84b9be3894ee351f968c316f4b3c3b
HaiweiChen <635011609@qq.com>
2016-03-26 17:55:17 +0800

    2016年3月26日17:55:15

--- js/js.js
+++ js/js.js
@@ -1,4 +1,21 @@
 // JavaScript Document
+var body=$('body');
 $('.menu a').click(function(){
 	$(this).addClass('menu-on').siblings().removeClass('menu-on');
+	$('.LOGO-wrapper').hide()
+	var text = $(this).text();
+	$('.'+text).show();
+	if(text=="Home"){
+		body.css('background','#90B44B');
+	}else if(text=='WeChat'){
+		body.css('background','#DDD23B');
+	}else if(text=='GitHub'){
+		body.css('background','#81C7D4');
+	}else if(text=='WeiBo'){
+		body.css('background','#ddd');
+	}else if(text=='ZhiHu'){
+		body.css('background','#2EA9DF');
+	}else if(text=="E-mail"){
+		body.css('background','#F19483');
+	}
 });
