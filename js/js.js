8b06ee32036fa67ffac85749b7e6225bbbeb4328
HaiweiChen <635011609@qq.com>
2016-04-08 16:15:07 +0800

    2016-4-8 16:15:04

--- js/js.js
+++ js/js.js
@@ -36,24 +36,33 @@ $('.skill li').mouseout(function(){
 });
 //canvas绘制小三角形
 $(function(){
-	var cxt =document.getElementById('triangle1').getContext("2d");//定义绘图环境
+	var cxt =document.getElementById('triangle').getContext("2d");//定义绘图环境
 	//cxt.beginPath();
 	cxt.fillStyle = '#fff';//定义填充颜色
 	cxt.strokeStyle="#fff";/*设置边框*/
 	cxt.lineWidth=1;/*边框的宽度*/
 	cxt.moveTo(150,0);
-	cxt.lineTo(135,20);
-	cxt.lineTo(165,20);
+	cxt.lineTo(130,150);
+	cxt.lineTo(170,150);
 	cxt.stroke();
 	cxt.fill();
 });
 //专业技能按钮hover事件
-$('.mini-block a').eq(0).mouseover(function(){
-	$('.show-block').animate({left:'1.5%'},'0');
-});
-$('.mini-block a').eq(1).mouseover(function(){
-	$('.show-block').animate({left:'35%'},'0');
-});
-$('.mini-block a').eq(2).mouseover(function(){
-	$('.show-block').animate({left:'67.5%'},'0');
+$('.mini-block a').click(function(){
+
+	var alt = $(this).attr('alt');
+	if(alt == 'HTML'){
+		$('.show-block').animate({left:'1.5%'},'0');
+	}else if(alt == 'script'){
+		$('.show-block').animate({left:'35%'},'0');
+	}else if(alt == 'other'){
+		$('.show-block').animate({left:'67.5%'},'0');
+	}
+	$(this).parent().addClass('big').siblings().removeClass('big');
+	setTimeout(function(){
+		if($('#'+alt).is(':hidden')){
+			$('.show-block ul').fadeOut(100);
+			setTimeout(function(){$('#'+ alt).fadeIn(1000)},100);
+		}
+	},300);
 });
